56042bc1a56888769c73bd19dd0345ae3ba73f0d
Jessica Stephens <jeslstep1@gmail.com>
2019-01-28 15:43:26 -0600

    changed card link styling

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import {connect} from 'react-redux';
@@ -41,17 +41,18 @@ class ProjectCard extends Component {
                 title={project.name}
               />
               <CardContent>
-                <h2 id="giveMargin">{project.name}</h2>
-                <div >
-                <p className="divforcard">{project.description}</p>
+                <h2>{project.name}</h2>
+                <div className="divforcard">
+                <p>{project.description}</p>
                 </div>
               </CardContent>
-            <CardActions className="divforcard2">
+            <div className="divforcard">
              
-              <a target="_blank" rel="noopener noreferrer" href={project.website}>Website</a>
-              <a target="_blank" rel="noopener noreferrer" href={project.github}> Github</a>
+              <Button  color="secondary"><a className="a" target="_blank" rel="noopener noreferrer" href={project.website}>Website</a></Button>
+               <a className="b" target="_blank" rel="noopener noreferrer" href={project.website}>|</a>
+              <Button color="secondary"><a className="a" target="_blank" rel="noopener noreferrer" href={project.github}> Github</a></Button>
              
-            </CardActions>
+            </div>
           </Card>
           </section>
         ))}
